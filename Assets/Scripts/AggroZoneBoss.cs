eedb9978a777ef08da28674314e7e2023670abb5
Xaqq <kapp.arno@gmail.com>
2013-11-07 03:31:00 -0500

    boss

---
+++ Assets/Scripts/AggroZoneBoss.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AggroZoneBoss : MonoBehaviour {
+	
+	public bossScript _monster;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider target)
+	{
+		playerController _target = target.GetComponent<playerController>();
+		if (_target != null)
+			_monster.SetTarget(_target);
+	}
+	
+	void OnTriggerExit(Collider target)
+	{
+		playerController _target = target.GetComponent<playerController>();
+		if (_target != null)
+			_monster.SetTarget(null);
+	}
+}
