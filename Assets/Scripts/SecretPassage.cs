c3db5abe1d7d34376a04d5c02ce474bf831ea06a
Alexandre ERNY <erny_a@airdealexandre.home>
2013-11-06 22:35:12 -0500

    tamere le prefab

---
+++ Assets/Scripts/SecretPassage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretPassage : MonoBehaviour {
+	
+	bool _isInside = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.GetComponent<playerController>() != null)
+		{
+			print (2 * (transform.position - collider.transform.position));
+			collider.transform.position = collider.transform.position + (2 * (transform.position - collider.transform.position));
+			collider.transform.position = new Vector3(collider.transform.position.x, 0 , collider.transform.position.z);
+		}
+	}
+}
